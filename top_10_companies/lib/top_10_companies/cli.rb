2096932579c872c93c3b6f40f2a1afe52245ac75
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-23 21:49:06 +0000

    automatically backed up by learn

--- top_10_companies/lib/top_10_companies/cli.rb
+++ top_10_companies/lib/top_10_companies/cli.rb
@@ -3,7 +3,7 @@ class Top10Companies::CLI
   def welcome
     puts "Welcome to the Top 10 list of Best Companies to Work For in 2018!"
 
-    Scraper.scrape_companies
+    Top10Companies::Scraper.scrape_companies
 
   end
 
